2dfbd2ee7374c611844b7eea2b176d02aeb0f99b
Wvader <34067397+wvader@users.noreply.github.com>
2023-02-17 22:05:21 +0000

    solution

--- hello-svelte/src/routes/+page.svelte
+++ hello-svelte/src/routes/+page.svelte
@@ -1,2 +1,3 @@
 <h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="/my-bug">the bug</a></p>
+<p>Visit <a href="/dynamic-route">the dynamic route 1</a> or the <a href="/dynamic-route-2">the dynamic route 2</a></p>
+<p>Visit the <a href="/world">world</a></p>
